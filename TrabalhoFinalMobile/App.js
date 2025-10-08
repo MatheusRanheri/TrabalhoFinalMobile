489278380ce04ed054dffd675c0c4e3fcf51954d
MatheusRanheri <matheusranheri@gmail.com>
2025-10-07 21:39:06 -0300

    Add files via upload

---
+++ TrabalhoFinalMobile/App.js
@@ -0,0 +1,27 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from 'react-native';
+
+import Home from "./Telas/Home";
+import Login from "./Telas/Login";
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Login" component={Login}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
