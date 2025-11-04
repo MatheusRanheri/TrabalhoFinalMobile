09b8396825d5b1016f13f2cf5f1b0617229017eb
MatheusRanheri <matheusranheri@gmail.com>
2025-11-04 17:33:29 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+import Home from "./Telas/Home/Home";
+import Login from "./Telas/Login/Login";
+import ResetarSenha from "./Telas/ResetarSenha/ResetarSenha";
+import CadastroCliente from "./Telas/CadastroCliente/CadastroCliente";
+import ValorFinal from "./Telas/ValorFinal/ValorFinal";
+import CadastrarValorHora from "./Telas/ValorHora/CadastrarValorHora";
+import Saida from "./Telas/Saida/Saida";
+import CadastroEstacionamento from "./Telas/CadastroEstacionemento/CadastroEstacionento";
+import valorDia from "./Telas/ValorDia/ValorDia";
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login}/>
+          <Stack.Screen name="Home" component={Home}/>
+          <Stack.Screen name="ResetarSenha" component={ResetarSenha}/>
+          <Stack.Screen name="ValorFinal" component={ValorFinal}/>
+          <Stack.Screen name="CadastrarValorHora" component={CadastrarValorHora}/>
+          <Stack.Screen name="Saida" component={Saida}/>
+          <Stack.Screen name="CadastroCliente" component={CadastroCliente}/>
+          <Stack.Screen name="CadastroEstacionamento" component={CadastroEstacionamento}/>
+          <Stack.Screen name="ValorDia" component={valorDia}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
